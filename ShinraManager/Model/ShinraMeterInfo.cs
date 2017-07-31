8459f3434b545fdfd7b853bbcfa58ce947cb6880
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-07-31 14:15:16 +0300

    Models for future use

---
+++ ShinraManager/Model/ShinraMeterInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShinraManager.Model
+{
+    internal class ShinraMeterInfo
+    {
+        public string ShinraPath;
+        public string ShinraDefaultName;
+        public string ShinraProcessName;
+        public bool ShinraMeterRunWithTera;
+    }
+}
---
+++ ShinraManager/Model/TccInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShinraManager.Model
+{
+    internal class TccInfo
+    {
+        public string TccPath;
+        public string TccDefaultName;
+        public string TccProcessName;
+        public bool TccRunWithTera;
+    }
+}
