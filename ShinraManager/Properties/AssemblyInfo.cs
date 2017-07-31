8a21abfdbdbbd0a405acc4a0fdeb44e7e9907fcf
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-07-31 14:18:28 +0300

    Project update, version change

--- ShinraManager/Properties/AssemblyInfo.cs
+++ ShinraManager/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.15")]
-[assembly: AssemblyFileVersion("1.0.0.15")]
+[assembly: AssemblyVersion("1.2.3.10")]
+[assembly: AssemblyFileVersion("1.2.3.10")]
