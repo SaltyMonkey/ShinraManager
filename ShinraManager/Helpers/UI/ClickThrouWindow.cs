955955333379bff762a33e59db6ec372b48adc93
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-10-03 02:41:08 +0300

    ClickThrouWindow refactoring

--- ShinraManager/Helpers/UI/ClickThrouWindow.cs
+++ ShinraManager/Helpers/UI/ClickThrouWindow.cs
@@ -192,37 +192,5 @@ namespace ShinraManager.UI
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SetForegroundWindow(IntPtr hWnd);
     }
-    public static class WindowsServices
-    {
-        private const int WS_EX_TRANSPARENT = 0x00000020;
-        private const int GWL_EXSTYLE = -20;
-
-        [DllImport("user32.dll")]
-        private static extern int GetWindowLong(IntPtr hwnd, int index);
-
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
-
-        public static void SetWindowExTransparent(IntPtr hwnd)
-        {
-            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
-        }
-
-        public static void SetWindowExVisible(IntPtr hwnd)
-        {
-            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle & ~WS_EX_TRANSPARENT);
-        }
-
-        public static IntPtr ClickNoFocus(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            const int WM_MOUSEACTIVATE = 0x0021;
-            const int MA_NOACTIVATE = 3;
-
-            if (msg != WM_MOUSEACTIVATE) { return IntPtr.Zero; }
-            handled = true;
-            return new IntPtr(MA_NOACTIVATE);
-        }
-    }
+   
 }
