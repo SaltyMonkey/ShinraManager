3c32bd4364d50c92cc087f78193e6633489e0b6a
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-10-02 22:11:36 +0300

    tray start recovered

--- ShinraManager/App.xaml.cs
+++ ShinraManager/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ShinraManager.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,57 @@ namespace ShinraManager
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex _mutex = null;
+        private string _appNameForActivate = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+        private bool _newInst;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            SingleInstanceRunCheck();
+
+            MainWindow wnd = new MainWindow();
+            if (e.Args.Length > 0)
+            {
+                if (e.Args[0] == "-minimized")
+                {
+                    wnd.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    wnd.Show();
+                }
+            }
+            else
+            {
+                wnd.Show();
+            }
+        }
+
+        private void SingleInstanceRunCheck()
+        {
+            _mutex = new Mutex(true, _appNameForActivate, out _newInst);
+            if (!_newInst)
+            {
+                ActivateFirstInst();
+                Current.Shutdown();
+            }
+        }
+
+        private void ActivateFirstInst()
+        {
+            try
+            {
+                var ptr = UnsafeAPI.FindWindow(null, System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+                if (ptr != IntPtr.Zero)
+                {
+                    UnsafeAPI.SetForegroundWindow(ptr);
+                    if (UnsafeAPI.IsIconic(ptr))
+                        UnsafeAPI.OpenIcon(ptr);
+                }
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
     }
 }
