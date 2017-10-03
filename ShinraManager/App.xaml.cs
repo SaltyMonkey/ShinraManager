60e9a65bcfc1c9c394ca9c44b639c1c16d7dd4fd
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-10-03 03:37:55 +0300

    unused directives in App.xaml.cs

--- ShinraManager/App.xaml.cs
+++ ShinraManager/App.xaml.cs
@@ -1,11 +1,6 @@
 ﻿using ShinraManager.Helpers;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace ShinraManager
